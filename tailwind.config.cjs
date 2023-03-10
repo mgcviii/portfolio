82f0c5b2927e012d58baf34bbc115607fa0962c2
mgcviii <111751220+mgcviii@users.noreply.github.com>
2023-03-10 16:56:49 +0100

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#F5F5F5",
+      beige: "#D1BF9B",
+      mocha: "#948C7F",
+      graphite: "#31302C",
+      creater_brown: "#47443D",
+    },
+    extend: {
+      fontFamily: {
+        otr: ["otr", "sans-serif"],
+        argentum: ["Argentum Sans", "sans-serif"]
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
